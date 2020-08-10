323e85cee081bc13d0872eb494c16dd87c9ab976
AlexandrK <alex.kalaidzhi@gmail.com>
2020-08-10 12:08:46 +0300

    Fixed bug with undefined filePath variable in Service::save method

--- src/Service.php
+++ src/Service.php
@@ -44,7 +44,7 @@ class Service
         return $this;
     }
 
-    public function save($filepath = 'export.pdf')
+    public function save($filePath = 'export.pdf')
     {
         if(file_put_contents($filePath, $this->exportContent)) {
             return $this;
@@ -82,10 +82,10 @@ class Service
         header("Content-Disposition: $disposition; filename=\"$filename\"");
         header("Content-Transfer-Encoding: binary");
         header("Content-Length: " . strlen($this->exportContent));
-        
+
         echo $this->exportContent;
 
         return $this;
     }
-    
+
 }
