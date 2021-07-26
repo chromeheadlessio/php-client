b8488cd7f0f9fd4235031b6e2438cac0c8cf7d38
dongnl <nguyenluongdong@gmail.com>
2021-07-26 16:04:50 +0700

    1. Add exception catcher for url_get_content. 2. Fix handler's save's $filePath case sensitivity typo.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,16 @@
 # Change Log
 
+## Version 1.2.0
+1. Add exception catcher for url_get_content.
+2. Fix handler's save's $filePath case sensitivity typo.
+
 ## Version 1.1.0
 
 1. Replace url in every css file with downloaded files using relative path from the css file.
 
 ## Version 1.0.0
 
-1. Replace file_get_contents with general url_get_contents using file_get_contents, fopen, or curl
+1. Replace file_get_contents with general url_get_contents using file_get_contents, fopen, or curl.
 
 ## Version 0.7.0
 
--- src/Exporter.php
+++ src/Exporter.php
@@ -7,22 +7,27 @@ class Exporter
 
     static function url_get_contents($url)
     {
-        if (function_exists('file_get_contents')) {
-            $url_get_contents_data = file_get_contents($url);
-        } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
-            $handle = fopen($url, "r");
-            $url_get_contents_data = stream_get_contents($handle);
-        } elseif (function_exists('curl_exec')) {
-            $conn = curl_init($url);
-            curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, true);
-            curl_setopt($conn, CURLOPT_FRESH_CONNECT,  true);
-            curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-            $url_get_contents_data = (curl_exec($conn));
-            curl_close($conn);
-        } else {
-            $url_get_contents_data = false;
+        try {
+            if (function_exists('file_get_contents')) {
+                $url_get_contents_data = file_get_contents($url);
+            } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
+                $handle = fopen($url, "r");
+                $url_get_contents_data = stream_get_contents($handle);
+            } elseif (function_exists('curl_exec')) {
+                $conn = curl_init($url);
+                curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, true);
+                curl_setopt($conn, CURLOPT_FRESH_CONNECT,  true);
+                curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+                $url_get_contents_data = (curl_exec($conn));
+                curl_close($conn);
+            } else {
+                $url_get_contents_data = false;
+            }
+            return $url_get_contents_data;
+        } catch (\Exception $e) {
+            // var_dump($e); exit;
         }
-        return $url_get_contents_data;
+        return "";
     }
 
     static function get($arr, $keys, $default = null)
